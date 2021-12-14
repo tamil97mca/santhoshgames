e6cbcc4f81ea098e875dc3b6c9d5cf95c53cbcf9
Tamil Selvan K <tamil97mca@gmail.com>
2021-12-14 08:50:50 +0530

    second commit

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  title: string = 'Category';
+
+  listOfCategory = [];
+
+  constructor() 
+  { 
+    this.listOfCategory = [
+      {categoryName: 'Latest Games', view: 'view'},
+      {categoryName: 'Most popular', view: 'view'},
+      {categoryName: 'Best Sellings', view: 'view'},
+      {categoryName: 'Pc Games', view: 'view'},
+      {categoryName: 'Ps2 Games', view: 'view'},
+      {categoryName: 'Ps3 Games', view: 'view'},
+      {categoryName: 'Ps4 Games', view: 'view'},
+      {categoryName: 'Ps5 Games', view: 'view'},
+      {categoryName: 'XBox Games', view: 'view'},
+      {categoryName: 'Game Accessories', view: 'view'},
+    ];
+  }
 
   ngOnInit() {
   }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  navpath = [];
+  constructor() 
+  { 
+    this.navpath = [
+      {routerLink: '/home', routerLinkActive: 'active', DisplayName: 'Home'},
+      {routerLink: '/about', routerLinkActive: 'active', DisplayName: 'About Us'},
+      {routerLink: '/contact', routerLinkActive: 'active', DisplayName: 'Contact Us'},
+    ]
+  }
 
   ngOnInit() {
   }
 
+  login()
+  {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+      else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })  }
 }
